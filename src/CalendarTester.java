6830d93cecd33e1eab6d32eae1a0233ac2db2282
Karthik-Tella <karthiktel11@gmail.com>
2017-08-06 19:29:54 -0700

    basic comments

--- src/CalendarTester.java
+++ src/CalendarTester.java
@@ -1,5 +1,13 @@
-
+/**
+ * The tester class of the calendar app
+ * @author Karthik Tella
+ *
+ */
 public class CalendarTester {
+	/**
+	 * Main method calls the model to initiate the app
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Model mv = new Model();
 	}
